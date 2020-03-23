3ea301d5844d4d2432472bc72683925daa276c28
Bartłomiej Mazur <ab.mazur@gmail.com>
2020-03-23 11:56:38 +0100

    Project's folder structure

---
+++ src/pages/Root.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Root = props => {
+  return (
+    <div>
+      Hello
+    </div>
+  )
+}
+
+Root.propTypes = {
+
+}
+
+export default Root
